664f60d6abc70a2d3b5dcde08471b88782ebd5b9
Caio Azevedo <87204701+CaioACL@users.noreply.github.com>
2021-07-09 16:59:12 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var operacao = prompt("Esse é: 1 - Zapdos, 2 - Lugia, 3 - Charizard, 4 - Pikachu ")
+
+if (operacao == 1) {
+  document.write("<h2>Você errou, esse é um Charizard!</h2>")
+} else if (operacao == 2) {
+  document.write("<h2>Você errou, esse é um Charizard!</h2>")
+} else if (operacao == 3) {
+  document.write("<h2>Parabéns, isso mesmo, é um Charizard!</h2>")
+} else if (operacao == 4) {
+  document.write("<h2>Você errou, esse é um Charizard!</h2>")
+} else {
+  document.write("<h2>Opção Inválida!</h2>")
+}
+  
+
+//Escrevendo na tela - document.write()
+//Concatenação (juntar palavras com variaveis) - ("palavra" + variavel)
+// == - comparação é diferente do = que usamos para atribuição
+
+//if = se
+//else = senão
+//else if = senão se
